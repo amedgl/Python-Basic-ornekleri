aaad0c68e966204940d72f51e0835a3857639f1c
amedgl <151673707+amedgl@users.noreply.github.com>
2024-06-22 00:30:41 +0300

    hesapmakinesi.py
    basit hesap makinesi algoritması.

---
+++ hesap makinesi.py
@@ -0,0 +1,50 @@
+while True:
+    print("""
+        1.Toplama işlemi
+        2.Çıkarma işlemi
+        3.Çarpma işlemi
+        4.Bölme işlemi
+    """)
+    islem=int(input("işlem yapmak için lütfen bir değer giriniz:"))
+    if islem==1:
+        sayi1=float(input("1.değeri giriniz:"))
+        sayi2=float(input("2.değeri giriniz:"))
+        toplam=sayi1+sayi2
+        print("işlem sonuçu:",toplam)
+        cikmak=input("işlemden çıkmak istiyormusunuz? E/H:").upper()
+        if cikmak=="E":
+            break
+        else:
+            pass
+    elif islem==2:
+        sayi1=float(input("1.değeri giriniz:"))
+        sayi2=float(input("2.değeri giriniz:"))
+        cikarma=sayi1-sayi2
+        print("işlem sonuçu:",cikarma)
+        cikmak=input("işlemden çıkmak istiyormusunuz? E/H:").upper()
+        if cikmak=="E":
+            break
+        else:
+            pass
+    elif islem==3:
+        sayi1=float(input("1.değeri giriniz:"))
+        sayi2=float(input("2.değeri giriniz:"))
+        carpma=sayi1*sayi2
+        print("Girdiğiniz değerlerin toplamı:",carpma)
+        cikmak=input("işlemden çıkmak istiyormusunuz? E/H:").upper()
+        if cikmak=="E":
+            break
+        else:
+            pass
+    elif islem==4:
+        sayi1=float(input("1.değeri giriniz:"))
+        sayi2=float(input("2.değeri giriniz:"))
+        bolme=sayi1/sayi2
+        print("Girdiğiniz değerlerin toplamı:",bolme)
+        cikmak=input("işlemden çıkmak istiyormusunuz? E/H:").upper()
+        if cikmak=="E":
+            break
+        else:
+            pass
+    else:
+        print("Geçersiz değer girdiniz lütfen geçerli bir değer giriniz..!")
